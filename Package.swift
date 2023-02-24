018719a39c8c08e44eca3e48a1adb51caa114c73
Wissam Alfreijat <wissam@colibrio.com>
2023-02-24 14:34:23 +0100

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "ColibrioReader",
+    platforms: [
+        .iOS(.v11),
+    ],
+    products: [
+        .library(
+            name: "ColibrioReader",
+            targets: ["ColibrioReader"]),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "ColibrioReader",
+            url: "https://spm.pkg.colibrio.com/framework/ColibrioReader-3.4.0.xcframework.zip",
+            checksum: "8fab25ed9cb4476a985162252085b71359176344c3e39450696237278cf83335"),
+    ]
+)
