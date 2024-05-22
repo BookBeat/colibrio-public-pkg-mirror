7dcba48bb5d726f40659c55155020f86a7212b35
Bitrise <ios-develop@colibrio.com>
2024-05-22 08:26:52 +0000

    [Bitrise] Update manifest for 3.11.0

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ColibrioReader",
-            url: "https://spm.pkg.colibrio.com/framework/ColibrioReader-3.10.1.xcframework.zip",
-            checksum: "bd6707dcfa9d001ac2e0dc9aa6f30dad7846ba2ae1fd64b3d329145327e24f49"),
+            url: "https://spm.pkg.colibrio.com/framework/ColibrioReader-3.11.0.xcframework.zip",
+            checksum: "5a102b31dcf7dbbf3765184d5168edd0b6b63e8de14fbf509626090c29aad8dd"),
     ]
 )
